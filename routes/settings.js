b6bc06890169aa4ec4f6f9be3159262f732daa81
Pablo Cordova <pccm_77@hotmail.com>
2017-12-01 22:40:05 -0500

    ADD print endpoint
    Add print endpoint refactoring to reduce code

--- routes/settings.js
+++ routes/settings.js
@@ -16,6 +16,8 @@ var oauth2Client = new OAuth2(
   'OUG7KOIqd_mV1517jEwAkMku',
   redirect_url
 );
+const printerID = 'bfeafc3e-9eaf-0709-5f42-20866d29b063';
+const printerProxy = 'D0E6B1C4-4B63-489B-96B7-3051A1821BAF';
 
 var url = oauth2Client.generateAuthUrl({
   access_type: 'offline',
@@ -82,48 +84,55 @@ router.post(
   }
 );
 
-router.get(
-  '/printerinfo',
+/*
+router.post(
+  '/print',
   passport.authenticate('jwt', { session: false }),
   haspermission,
   (req, res) => {
+*/
+router.post(
+  '/print/sale',
+  async (req, res) => {
 
-    Setting.find({})
-      .then(setting => {
-        const refreshTokenGoogle = setting[0].refreshTokenGoogle;
-        const expirationTokenGoogle = setting[0].expirationTokenGoogle;
-        const tokenGoogle = setting[0].tokenGoogle;
+    //const saleID = req.body.saleID;
+    const tickeProperties = {
+      'version': '1.0',
+      'print': {
+        'vendor_ticket_item': [],
+        'color': { 'type': 'STANDARD_MONOCHROME' },
+        'copies': { 'copies': 1 }
+      }
+    };
 
-        const dateToday = new Date();
-        // 1 minute forward to avoid exact time
-        const dateTodayPlus1Minute = moment(dateToday).add(1, 'm').toDate();
-
-        const dateExpiration = new Date(expirationTokenGoogle);
-
-        console.log('Important data to nalize');
-        console.log(dateExpiration);
-        console.log(dateTodayPlus1Minute);
-
-        // Case date expiration, get new token
-        if (dateExpiration < dateTodayPlus1Minute) {
-          console.log('Updating access token');
-          oauth2Client.credentials['refresh_token'] = refreshTokenGoogle;
-          oauth2Client.refreshAccessToken(function(err, tokens) {
-            // Save new token and new expiration
-            let setting = new Setting();
-            setting.expirationTokenGoogle = tokens.expiry_date;
-            setting.tokenGoogle = tokens.access_token;
-
-            setting.save();
-
-            return getPrinterInfo(res, tokens.access_token);
-          });
-
-        } else {
-          console.log('Using old access token');
-          return getPrinterInfo(res, tokenGoogle);
+    // First I'll check if printer is available and with also I'll get the xsrf-token
+    const accessToken = await getTokenGoogleUpdated();
+    //console.log('printing access token google');
+    //console.log(accessToken);
+    //return getPrinterInfo(res, accessToken);
+    axios.get(
+      'https://www.google.com/cloudprint/submit',
+      {
+        params: {
+          printerid : printerID,
+          title: 'title printer',
+          ticket: tickeProperties,
+          content : 'to test jejejje pleaseeee',
+          contentType: 'text/plain'
+        },
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
         }
 
+      }
+
+    )
+      .then(response => {
+        console.log('google cloud print document');
+        return res.status(config.STATUS.OK).send({
+          result: response.data,
+          message: config.RES.OK,
+        });
       })
       .catch(err => {
         return res.status(config.STATUS.SERVER_ERROR).send({
@@ -135,17 +144,63 @@ router.get(
   }
 );
 
+async function getTokenGoogleUpdated() {
+
+  return await Setting.find({})
+    .then(async setting => {
+      const refreshTokenGoogle = setting[0].refreshTokenGoogle;
+      const expirationTokenGoogle = setting[0].expirationTokenGoogle;
+      const tokenGoogle = setting[0].tokenGoogle;
+
+      const dateToday = new Date();
+      // 1 minute forward to avoid exact time
+      const dateTodayPlus1Minute = moment(dateToday).add(1, 'm').toDate();
+
+      const dateExpiration = new Date(expirationTokenGoogle);
+
+      console.log('Important data to nalize');
+      console.log(dateExpiration);
+      console.log(dateTodayPlus1Minute);
+
+      // Case date expiration, get new token
+      if (dateExpiration < dateTodayPlus1Minute) {
+        console.log('Updating access token');
+        oauth2Client.credentials['refresh_token'] = refreshTokenGoogle;
+        return await oauth2Client.refreshAccessToken( async function(err, tokens) {
+          // Save new token and new expiration
+          //let setting = new Setting();
+          setting[0].expirationTokenGoogle = tokens.expiry_date;
+          setting[0].tokenGoogle = tokens.access_token;
+
+          await setting[0].save();
+
+          //return getPrinterInfo(res, tokens.access_token);
+          return tokens.access_token;
+        });
+
+      } else {
+        console.log('Using old access token');
+        //return getPrinterInfo(res, tokenGoogle);
+        return tokenGoogle;
+      }
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+}
+
 function getPrinterInfo(res, accessToken) {
 
-  const googleToken = accessToken;
   axios.get(
     'https://www.google.com/cloudprint/list',
     {
       params: {
-        proxy: '9ff10dfa-866d-40a5-afbe-563771671def'
+        proxy: printerProxy
       },
       headers: {
-        'Authorization': 'Bearer ' + googleToken
+        'Authorization': 'Bearer ' + accessToken
       }
     }
   )
